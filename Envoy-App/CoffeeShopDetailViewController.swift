c4e79d3e6ceaabc842da33b2534725f0b5bf7cd3
Isaac Rosenberg <irosenb7@gmail.com>
2020-02-12 13:36:45 -0800

    Coffee shop detail, added tests

---
+++ Envoy-App/CoffeeShopDetailViewController.swift
@@ -0,0 +1,32 @@
+//
+//  CoffeeShopDetailViewController.swift
+//  Envoy-App
+//
+//  Created by Isaac Rosenberg on 2/12/20.
+//  Copyright © 2020 Isaac Rosenberg. All rights reserved.
+//
+
+import UIKit
+
+class CoffeeShopDetailViewController: UIViewController {
+    var coffeeShop: CoffeeShop?
+    let image = UIImageView()
+    let 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Envoy-AppTests/CoffeeShopTests.swift
+++ Envoy-AppTests/CoffeeShopTests.swift
@@ -7,9 +7,10 @@
 //
 
 import XCTest
+@testable import Envoy_App
 
 class CoffeeShopTests: XCTestCase {
-
+    let shopCount = 15
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -18,16 +19,21 @@ class CoffeeShopTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testCoffeeShopsNotNil() {
+        Envoy_App.CoffeeShop.fetch { (shops) in
+            XCTAssertNotNil(shops)
         }
     }
-
+    
+    func testCoffeeShopsCount() {
+        Envoy_App.CoffeeShop.fetch { (shops) in
+            XCTAssertTrue(shops?.count == self.shopCount)
+        }
+    }
+    
+    func testCoffeeShopsFirstShop() {
+        Envoy_App.CoffeeShop.fetch { (shops) in
+            XCTAssertTrue(shops?.first?.name == "Philz Coffee")
+        }
+    }
 }
